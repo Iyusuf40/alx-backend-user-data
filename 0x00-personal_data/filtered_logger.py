8bd498d1f170d58c3eec88ebe835f6821164db09
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-01 22:40:33 +0000

    implement format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ mod doc str """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -9,3 +10,23 @@ def filter_datum(fields: List[str], redaction: str,
     """ filter_datum doc str """
     pat = '(?P<nm>' + '=|'.join(fields) + '=)' + f'[^{separator}]+{separator}'
     return re.sub(pat, r"\g<nm>{}{}".format(redaction, separator), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ func doc str """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+            )
+        return super().format(record)

bd74560d09f4c26653e78dff38efbc8e1361010c
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-01 20:02:47 +0000

    reduce line count

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,5 @@ import re
 
 def filter_datum(fields, redaction, message, separator) -> str:
     """ filter_datum doc str """
-    repl = r"\g<nm>{}".format(redaction)
     pat = '(?P<nm>' + '=|'.join(fields) + '=)' + '[^;]*'
-    return re.sub(pat, repl, message)
+    return re.sub(pat, r"\g<nm>{}".format(redaction), message)

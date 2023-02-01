0b1b7e9c8e2e1da06f33f9ce520caaa795ab3d82
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-01 20:33:20 +0000

    make executable

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,11 @@
 
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
     """ filter_datum doc str """
     pat = '(?P<nm>' + '=|'.join(fields) + '=)' + f'[^{separator}]+{separator}'
     return re.sub(pat, r"\g<nm>{}{}".format(redaction, separator), message)

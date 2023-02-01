8ae6c2682078d20532f3d11ecc19ed160ba6b493
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-01 19:58:05 +0000

    test

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,11 +5,8 @@
 import re
 
 
-r = r"\1{}\3"
-
-
 def filter_datum(fields, redaction, message, separator) -> str:
     """ filter_datum doc str """
-    for f in fields:
-        message = re.sub(f"(.*{f}=)([^;]*)(.*)", r.format(redaction), message)
-    return message
+    repl = r"\g<nm>xxx"
+    pat = '(?P<nm>' + '=|'.join(fields) + '=)' + '[^;]*'
+    return re.sub(pat, repl, message)

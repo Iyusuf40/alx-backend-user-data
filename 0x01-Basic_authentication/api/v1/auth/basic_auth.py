b8abca7458894b220a582153040ce9db9b940e97
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-06 15:35:59 +0000

    add extract_user_credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,9 @@ Auth class defined here
 """
 from flask import abort, request
 from api.v1.auth.auth import Auth
+from typing import Tuple
+import base64
+import io
 
 
 class BasicAuth(Auth):
@@ -23,3 +26,36 @@ class BasicAuth(Auth):
             return None
 
         return comp[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """decode wrapper func"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+
+        try:
+            res = base64.b64decode(
+                bytes(base64_authorization_header, 'utf-8')
+            )
+            res = res.decode('utf-8')
+
+        except Exception as e:
+            # print(e)
+            res = None
+
+        return res
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> Tuple[str, str]:
+        """doc str"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) != str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':'))

62cdb8e16d7bfd13d4230f1479634f0df6fda82f
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-08 11:51:13 +0000

    implement timeout for sessions

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -20,11 +20,13 @@ def get_auth():
     from api.v1.auth.auth import Auth
     from api.v1.auth.basic_auth import BasicAuth
     from api.v1.auth.session_auth import SessionAuth
+    from api.v1.auth.session_exp_auth import SessionExpAuth
 
     auth_repo = {
         'auth': Auth,
         'basic_auth': BasicAuth,
-        'session_auth': SessionAuth
+        'session_auth': SessionAuth,
+        'session_exp_auth': SessionExpAuth
     }
 
     return auth_repo

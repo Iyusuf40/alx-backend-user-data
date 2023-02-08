0777b9e893e8e2fa66688bd24f23f0836d36f932
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-08 09:02:20 +0000

    update SessionAuth class: create_session method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,11 +5,22 @@ SessionAuth class defined here
 from flask import abort, request
 from api.v1.auth.auth import Auth
 from typing import Tuple, TypeVar
-# import base64
+import uuid
 from models.user import User
 
 
 class SessionAuth(Auth):
-    """ BasicAuth class """
+    """ SessionAuth class """
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a new session """
+        if not user_id:
+            return None
+        if type(user_id) != str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

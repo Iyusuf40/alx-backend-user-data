6e9553c52090661cfcb39ee0f8c96e3b09440f8f
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-14 07:48:34 +0000

    use Dict in place of Mapping

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from typing import TypeVar, Type, Mapping
+from typing import TypeVar, Type, Mapping, Dict
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
@@ -41,7 +41,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kw: Mapping) -> User:
+    def find_user_by(self, **kw: Dict[str, str]) -> User:
         ''' searches for user by kw '''
         valid_keys = ['email', 'id', 'hashed_password',
                       'session_id', 'reset_token']

b639a7040b206a4e0860fc5bfa1ab256645ad24f
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-14 07:25:21 +0000

    test

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,7 +8,7 @@ import uuid
 
 def _hash_password(password: str) -> bytes:
     """ returns a passwd hash """
-    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt(4))
+    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
 
 
 class Auth:

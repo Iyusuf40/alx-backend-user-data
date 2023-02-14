949f471b5210712198c5968090e68f30d00c29e2
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-02-14 08:11:43 +0000

    test

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,37 +45,51 @@ class DB:
         ''' searches for user by kw '''
         if not kw:
             raise InvalidRequestError
-        valid_keys = ['email', 'id', 'hashed_password',
-                      'session_id', 'reset_token']
-        query = self._session.query(User)
-        for key in kw:
-            if key == 'email':
-                query = query.filter(
-                            User.email == kw[key]
-                            )
-            elif key == 'id':
-                query = query.filter(
-                            User.id == kw[key]
-                            )
-            elif key == 'hashed_password':
-                query = query.filter(
-                            User.hashed_password == kw[key]
-                            )
-            elif key == 'session_id':
-                query = query.filter(
-                            User.session_id == kw[key]
-                            )
-            elif key == 'reset_token':
-                query = query.filter(
-                            User.reset_token == kw[key]
-                            )
-        all_users = query.all()
-        for key in kw:
-            if key not in valid_keys:
+
+        column_names = User.__table__.columns.keys()
+        for key in kw.keys():
+            if key not in column_names:
                 raise InvalidRequestError
-        if not all_users:
+
+        user = self._session.query(User).filter_by(**kw).first()
+
+        if user is None:
             raise NoResultFound
-        return all_users[0]
+
+        return user
+        # if not kw:
+        #     raise InvalidRequestError
+        # valid_keys = ['email', 'id', 'hashed_password',
+        #               'session_id', 'reset_token']
+        # query = self._session.query(User)
+        # for key in kw:
+        #     if key == 'email':
+        #         query = query.filter(
+        #                     User.email == kw[key]
+        #                     )
+        #     elif key == 'id':
+        #         query = query.filter(
+        #                     User.id == kw[key]
+        #                     )
+        #     elif key == 'hashed_password':
+        #         query = query.filter(
+        #                     User.hashed_password == kw[key]
+        #                     )
+        #     elif key == 'session_id':
+        #         query = query.filter(
+        #                     User.session_id == kw[key]
+        #                     )
+        #     elif key == 'reset_token':
+        #         query = query.filter(
+        #                     User.reset_token == kw[key]
+        #                     )
+        # all_users = query.all()
+        # for key in kw:
+        #     if key not in valid_keys:
+        #         raise InvalidRequestError
+        # if not all_users:
+        #     raise NoResultFound
+        # return all_users[0]
 
     def update_user(self, user_id: int, **kw: Mapping) -> None:
         """ updates a user """
